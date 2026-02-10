1d48f54638ce0404f451d42f85e27522c6d9588f
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-02-10 13:59:00 -0300

    add list certificates component and update styles

--- src/app/pages/certificates/certificates.ts
+++ src/app/pages/certificates/certificates.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
+import { ItemCertificate } from "../../_components/item-certificate/item-certificate";
 
 @Component({
   selector: 'app-certificates',
-  imports: [SecondaryButton],
+  imports: [SecondaryButton, ItemCertificate],
   templateUrl: './certificates.html',
   styleUrl: './certificates.css',
 })
