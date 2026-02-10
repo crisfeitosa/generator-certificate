1373dd8be5611e11265251543b1a92deba5feb15
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-02-10 13:25:24 -0300

    add secondary button component with styles and template

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Navbar } from "./_components/navbar/navbar";
 import { PrimaryButton } from './_components/primary-button/primary-button';
+import { SecondaryButton } from './_components/secondary-button/secondary-button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
