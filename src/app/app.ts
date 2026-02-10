7793fe3f9d65292fc7ad68729661f920bdd12974
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-02-10 13:41:44 -0300

    add base UI component with template and styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Navbar } from "./_components/navbar/navbar";
+import { BaseUi } from './_components/base-ui/base-ui';
 import { PrimaryButton } from './_components/primary-button/primary-button';
 import { SecondaryButton } from './_components/secondary-button/secondary-button';
 import { ItemCertificate } from './_components/item-certificate/item-certificate';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificate],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificate, BaseUi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
