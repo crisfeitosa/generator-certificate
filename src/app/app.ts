7a04180d167326404e87e6f67cf4fb657a95556f
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-02-10 13:37:40 -0300

    add item certificate component with template and styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { Navbar } from "./_components/navbar/navbar";
 import { PrimaryButton } from './_components/primary-button/primary-button';
 import { SecondaryButton } from './_components/secondary-button/secondary-button';
+import { ItemCertificate } from './_components/item-certificate/item-certificate';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificate],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
