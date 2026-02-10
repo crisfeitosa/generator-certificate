4c6ab5a8a502efde0cb5324fcb83a4afe8557b76
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-02-10 15:02:31 -0300

    feat: add certificate page with styles and SVG assets
    - Created certificate_signature.svg and logo.svg for visual elements.
    - Added certificate.css for styling the certificate page layout.
    - Implemented certificate.html structure with navigation and content areas.
    - Developed certificate.ts component to manage the certificate page logic.

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,13 +5,14 @@ import { Navbar } from "./_components/navbar/navbar";
 import { BaseUi } from './_components/base-ui/base-ui';
 import { Certificates } from './pages/certificates/certificates';
 import { CertificatesForm } from './pages/certificates-form/certificates-form';
+import { Certificate } from './pages/certificate/certificate';
 import { PrimaryButton } from './_components/primary-button/primary-button';
 import { SecondaryButton } from './_components/secondary-button/secondary-button';
 import { ItemCertificate } from './_components/item-certificate/item-certificate';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificate, BaseUi, Certificates, CertificatesForm],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificate, BaseUi, Certificates, CertificatesForm, Certificate],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
