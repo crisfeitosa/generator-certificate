469d1dd95e392a619f5cf2b4bbe128444a53a877
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-02-10 15:40:08 -0300

    refactor: update routing structure and clean up imports; streamline app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Certificate } from './pages/certificate/certificate';
+import { Certificates } from './pages/certificates/certificates';
+import { CertificatesForm } from './pages/certificates-form/certificates-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Certificates
+  },
+  {
+    path: 'certificates/new',
+    component: CertificatesForm
+  },
+  {
+    path: 'certificates/:id',
+    component: Certificate
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,16 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Navbar } from "./_components/navbar/navbar";
 import { BaseUi } from './_components/base-ui/base-ui';
-import { Certificates } from './pages/certificates/certificates';
-import { CertificatesForm } from './pages/certificates-form/certificates-form';
-import { Certificate } from './pages/certificate/certificate';
-import { PrimaryButton } from './_components/primary-button/primary-button';
-import { SecondaryButton } from './_components/secondary-button/secondary-button';
-import { ItemCertificate } from './_components/item-certificate/item-certificate';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificate, BaseUi, Certificates, CertificatesForm, Certificate],
+  imports: [RouterOutlet, Navbar, CommonModule, BaseUi,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
