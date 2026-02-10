96a33e7b34e6ffed929949f24bd36d0d6ac5960d
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-02-10 17:16:02 -0300

    feat: enhance navigation and routing; add router links to navbar and certificate components

--- src/app/_components/item-certificate/item-certificate.ts
+++ src/app/_components/item-certificate/item-certificate.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { SecondaryButton } from '../secondary-button/secondary-button';
 
 @Component({
   selector: 'app-item-certificate',
-  imports: [SecondaryButton],
+  imports: [SecondaryButton, RouterLink],
   templateUrl: './item-certificate.html',
   styleUrl: './item-certificate.css',
 })
 export class ItemCertificate {
-
+  id = '12345';
 }
--- src/app/pages/certificate/certificate.ts
+++ src/app/pages/certificate/certificate.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
 
 @Component({
   selector: 'app-certificate',
-  imports: [SecondaryButton],
+  imports: [SecondaryButton, RouterLink],
   templateUrl: './certificate.html',
   styleUrl: './certificate.css',
 })
