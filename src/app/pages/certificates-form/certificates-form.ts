9e45a996d7c446f5eb89852e43ded7195a0e14b1
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-02-11 09:10:39 -0300

    feat: bind input fields to component properties and update activity list rendering

--- src/app/pages/certificates-form/certificates-form.ts
+++ src/app/pages/certificates-form/certificates-form.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { PrimaryButton } from '../../_components/primary-button/primary-button';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-certificates-form',
-  imports: [PrimaryButton, SecondaryButton],
+  imports: [PrimaryButton, SecondaryButton, FormsModule],
   templateUrl: './certificates-form.html',
-  styleUrl: './certificates-form.css',
+  styleUrls: ['./certificates-form.css'],
 })
 export class CertificatesForm {
-
+  name: string = '';
+  activity: string = '';
+  activities: string[] = ['Angular', 'React', 'Vue'];
 }
