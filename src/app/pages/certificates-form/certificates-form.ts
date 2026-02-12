acdaa7dde70652f2279ec582ccb4ca43b5e28cd9
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-02-12 09:31:44 -0300

    feat: enhance form validation and error handling for input fields

--- src/app/pages/certificates-form/certificates-form.ts
+++ src/app/pages/certificates-form/certificates-form.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { PrimaryButton } from '../../_components/primary-button/primary-button';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-certificates-form',
-  imports: [PrimaryButton, SecondaryButton, FormsModule],
+  imports: [PrimaryButton, SecondaryButton, FormsModule, CommonModule],
   templateUrl: './certificates-form.html',
   styleUrls: ['./certificates-form.css'],
 })
@@ -13,4 +14,8 @@ export class CertificatesForm {
   name: string = '';
   activity: string = '';
   activities: string[] = ['Angular', 'React', 'Vue'];
+
+  invalidField(control: NgModel) {
+    return control.invalid && control.touched;
+  }
 }
