79aa0373e791c2b8d99bda1fe0ec326ee39ead3c
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-02-12 19:14:52 -0300

    feat: refactor certificate form to use Certificate interface; update form validation and activity management

--- src/app/pages/certificates-form/certificates-form.ts
+++ src/app/pages/certificates-form/certificates-form.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { PrimaryButton } from '../../_components/primary-button/primary-button';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
 import { FormsModule, NgModel } from '@angular/forms';
+import { Certificate } from '../../interfaces/certificate';
 
 @Component({
   selector: 'app-certificates-form',
@@ -11,24 +12,35 @@ import { FormsModule, NgModel } from '@angular/forms';
   styleUrls: ['./certificates-form.css'],
 })
 export class CertificatesForm {
-  name: string = '';
+  certificate: Certificate = {
+    name: '',
+    activities: [],
+  };
+
   activity: string = '';
-  activities: string[] = [];
 
   invalidField(control: NgModel) {
     return control.invalid && control.touched;
   }
 
   validateForm() {
-    return this.activities.length > 0 && this.name.length > 0;
+    return this.certificate.activities.length > 0 && this.certificate.name.length > 0;
   }
 
   addActivity() {
-    this.activities.push(this.activity);
+    this.certificate.activities.push(this.activity);
     this.activity = '';
   }
 
   removeActivity(index: number) {
-    this.activities.splice(index, 1);
+    this.certificate.activities.splice(index, 1);
+  }
+
+  submit() {
+    if(!this.validateForm()) {
+      return;
+    }
+
+    console.log(this.certificate);
   }
 }
